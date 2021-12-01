38004803966ee13684f570bc5bdffaf56fef303b
Varnita <varnita.makrariya@gmail.com>
2021-12-01 12:21:06 +0530

    Contact updated

--- frontend/src/component/layout/Contact/Contact.js
+++ frontend/src/component/layout/Contact/Contact.js
@@ -5,8 +5,17 @@ import { Button } from "@material-ui/core";
 const Contact = () => {
   return (
     <div className="contactContainer">
-      <a className="mailBtn" href="mailto:mymailforabhi@gmail.com">
+      <a className="mailBtn" href="mailto:crabthe64@gmail.com">
         <Button>Contact: varnita@gmail.com</Button>
+      </a>
+      <a className="mailBtn" href="mailto:crabthe64@gmail.com">
+        <Button>Contact: yashika@gmail.com</Button>
+      </a>
+      <a className="mailBtn" href="mailto:crabthe64@gmail.com">
+        <Button>Contact: vachas@gmail.com</Button>
+      </a>
+      <a className="mailBtn" href="mailto:crabthe64@gmail.com">
+        <Button>Contact: tarini@gmail.com</Button>
       </a>
     </div>
   );
